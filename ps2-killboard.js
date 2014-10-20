2fce28f00a2c2ab13b4adbc17393c2fd506ca2ba
mereth <mereth@users.noreply.github.com>
2014-10-20 15:41:11 +0200

    migrate character page to knockoutjs

--- ps2-killboard.js
+++ ps2-killboard.js
@@ -1,90 +1,99 @@
-var killboard = new function killboard() {
+(function() {
 	var self = this;
 	
-	var template = "<table>\
-<thead>\
-<tr>\
-	<th rowspan='2'>Time</th>\
-	<th rowspan='2'></th>\
-	<th colspan='6'>Player</th>\
-	<th rowspan='2'>Method</th>\
-</tr>\
-<tr>\
-	<th>Name</th>\
-	<th>BR</th>\
-	<th>Outfit</th>\
-	<th>K/D</th>\
-	<th>SPM</th>\
-	<th>KPM</th>\
-</tr>\
-</thead>\
-<tbody>\
-</tbody>\
-</table>\
-<button id='page_more'>More</button>\
-";
-
-	var _character_id = -1;
+    var url = "http://census.soe.com/get/ps2:v2/characters_event/?type=KILL,DEATH";
+    url += "&c:join=character^show:character_id'name.first'battle_rank.value'faction_id^inject_at:character";
+    url += "(";
+    url += "characters_stat_history^on:character_id^terms:stat_name=kills^show:all_time'week.w01'week.w02^inject_at:stat_kills";
+    url += ",characters_stat_history^on:character_id^terms:stat_name=deaths^show:all_time'week.w01'week.w02^inject_at:stat_deaths";
+    url += ",characters_stat_history^on:character_id^terms:stat_name=score^show:all_time'week.w01'week.w02^inject_at:stat_score";
+    url += ",characters_stat_history^on:character_id^terms:stat_name=time^show:all_time'week.w01'week.w02^inject_at:stat_time";
+    url += ",outfit_member^show:outfit_id^inject_at:outfit(outfit^show:name'alias^inject_at:details)";
+    url += ")";
+    url += "&c:join=character^on:attacker_character_id^to:character_id^show:character_id'name.first'battle_rank.value'faction_id^inject_at:attacker_character";
+    url += "(";
+    url += "characters_stat_history^on:character_id^terms:stat_name=kills^show:all_time'week.w01'week.w02^inject_at:stat_kills";
+    url += ",characters_stat_history^on:character_id^terms:stat_name=deaths^show:all_time'week.w01'week.w02^inject_at:stat_deaths";
+    url += ",characters_stat_history^on:character_id^terms:stat_name=score^show:all_time'week.w01'week.w02^inject_at:stat_score";
+    url += ",characters_stat_history^on:character_id^terms:stat_name=time^show:all_time'week.w01'week.w02^inject_at:stat_time";
+    url += ",outfit_member^show:outfit_id^inject_at:outfit(outfit^show:name'alias^inject_at:details)";
+    url += ")";
+    url += "&c:join=item^on:attacker_weapon_id^to:item_id^show:name.en'image_path^inject_at:attacker_weapon";
+    url += "&c:join=vehicle^on:attacker_vehicle_id^to:vehicle_id^show:name.en'image_path^inject_at:attacker_vehicle";
+    url += "&callback=?";
+    
 	var _page_index = 1;
 	var _page_size = 100;
 	
+    var viewModel = {
+        characterId: ko.observable()
+        
+      , events: ko.observableArray()
+      
+      , getMore: function() {
+            _page_size += 100;
+            getKillboard();
+        }
+    };
+    
+    var getKillboard = function() {
+        $.getJSON(url, { character_id: viewModel.characterId(), 'c:start': _page_index, 'c:limit': _page_size }, buildKillboard);
+    };
+    
 	var buildKillboard = function buildKillboard(killboard) {
-		self.$tbody.empty();
+        viewModel.events.removeAll();
 		$.each(killboard.characters_event_list, buildKillboardEvent);
 	};
 
 	var buildKillboardEvent = function buildKillboardEvent(idx, event) {
-		var type = event.table_type;
-		var timestamp = event.timestamp;
+        var model = {};
+        
+		model.type = event.table_type;
+        
 		var character = event.character;
-		if(type === 'deaths') {
+		if(model.type === 'deaths') {
 			character = event.attacker_character;
 		}
 		
-		var suicide = 0;
+		model.suicide = 0;
 		// remove suicide kills event, keep only the death one and mark it as suicide
 		if(event.character_id == event.attacker_character_id) {
-			if(type == "kills") {
+			if(model.type == "kills") {
 				return;
 			}
 			else {
-				suicide = 1;
+				model.suicide = 1;
 			}
 		}
-		
-		var $tr = $('<tr></tr>')
-		
-		$tr.addClass(type);
-		if(suicide === 1) {
-			$tr.addClass("suicide");
-		}
+        
+		model.headshot = 0;
 		if(event.is_headshot === "1") {
-			$tr.addClass("headshot");
+			model.headshot = 1;
 		}
 		
+        model.faction = "";
 		if(character) {
 			if(character.faction_id == 1) {
-				$tr.addClass("vs");
+				model.faction = "vs";
 			}
 			else if(character.faction_id == 2) {
-				$tr.addClass("nc");
+				model.faction = "nc";
 			}
 			else if(character.faction_id == 3) {
-				$tr.addClass("tr");
+				model.faction = "tr";
 			}
 		}
 		
-		$tr.append('<td class="time">' + buildTimeInfo(timestamp) + '</td>');
-		$tr.append('<td class="event">&nbsp;</td>');
-		$tr.append('<td class="name">' + buildPlayerInfo(character) + '</td>');
-		$tr.append('<td class="br">' + buildBR(character) + '</td>');
-		$tr.append('<td class="outfit">' + buildOutfitInfo(character) + '</td>');
-		$tr.append('<td class="kd">' + buildKD(character) + '</td>');
-		$tr.append('<td class="spm">' + buildSPM(character) + '</td>');
-		$tr.append('<td class="kpm">' + buildKPM(character) + '</td>');
-		$tr.append('<td class="method">' + buildMethodInfo(event.attacker_weapon, event.attacker_vehicle) + '</td>');
+		model.time = buildTimeInfo(event.timestamp);
+		model.name = buildPlayerInfo(character);
+		model.br = buildBR(character);
+		model.outfit = buildOutfitInfo(character);
+		model.kd = buildKD(character);
+		model.spm = buildSPM(character);
+		model.kpm = buildKPM(character);
+		model.method = buildMethodInfo(event.attacker_weapon, event.attacker_vehicle);
 		
-		$tr.appendTo(self.$tbody);
+		viewModel.events.push(model);
 	};
 
 	var buildTimeInfo = function buildTimeInfo(timestamp) {
@@ -204,43 +213,15 @@ var killboard = new function killboard() {
 		return sum;
 	};
 	
-	return {
-		init: function init($container, character_id) {
-			$container.append(template);
-			self.$tbody = $container.find('tbody');
-			_character_id = character_id;
-			
-			this.update();
-			
-			var self2 = this;
-			$('#page_more').on('click', function() {
-				_page_size += 100;
-				self2.update();
-			});
-		}
-		,update: function update() {
-			var url = "http://census.soe.com/get/ps2:v2/characters_event/?type=KILL,DEATH";
-			url += "&c:join=character^show:character_id'name.first'battle_rank.value'faction_id^inject_at:character";
-			url += "(";
-			url += "characters_stat_history^on:character_id^terms:stat_name=kills^show:all_time'week.w01'week.w02^inject_at:stat_kills";
-			url += ",characters_stat_history^on:character_id^terms:stat_name=deaths^show:all_time'week.w01'week.w02^inject_at:stat_deaths";
-			url += ",characters_stat_history^on:character_id^terms:stat_name=score^show:all_time'week.w01'week.w02^inject_at:stat_score";
-			url += ",characters_stat_history^on:character_id^terms:stat_name=time^show:all_time'week.w01'week.w02^inject_at:stat_time";
-			url += ",outfit_member^show:outfit_id^inject_at:outfit(outfit^show:name'alias^inject_at:details)";
-			url += ")";
-			url += "&c:join=character^on:attacker_character_id^to:character_id^show:character_id'name.first'battle_rank.value'faction_id^inject_at:attacker_character";
-			url += "(";
-			url += "characters_stat_history^on:character_id^terms:stat_name=kills^show:all_time'week.w01'week.w02^inject_at:stat_kills";
-			url += ",characters_stat_history^on:character_id^terms:stat_name=deaths^show:all_time'week.w01'week.w02^inject_at:stat_deaths";
-			url += ",characters_stat_history^on:character_id^terms:stat_name=score^show:all_time'week.w01'week.w02^inject_at:stat_score";
-			url += ",characters_stat_history^on:character_id^terms:stat_name=time^show:all_time'week.w01'week.w02^inject_at:stat_time";
-			url += ",outfit_member^show:outfit_id^inject_at:outfit(outfit^show:name'alias^inject_at:details)";
-			url += ")";
-			url += "&c:join=item^on:attacker_weapon_id^to:item_id^show:name.en'image_path^inject_at:attacker_weapon";
-			url += "&c:join=vehicle^on:attacker_vehicle_id^to:vehicle_id^show:name.en'image_path^inject_at:attacker_vehicle";
-			url += "&callback=?";
-
-			$.getJSON(url, { character_id: _character_id, 'c:start': _page_index, 'c:limit': _page_size }, buildKillboard);
-		}
-	}
-}();
+    if(!window.ps2) window.ps2 = {};
+    window.ps2.killboard = {
+        update: function(id) {
+            viewModel.characterId(id);
+            getKillboard();
+        }
+    };
+    
+	$(function() {
+        ko.applyBindings(viewModel, document.getElementById('killboard'));
+    });
+})();
--- ps2-player.js
+++ ps2-player.js
@@ -1,158 +1,77 @@
-var player = new function player() {
-	var self = this;
-	
-	var build = function build(census_result) {
-		var character = census_result.character_list[0];
-		
-		document.title = character.name.first;
-		
-		self.$name.text(character.name.first);
-		if(character.outfit_member) {
-			var outfit = character.outfit_member.outfit
-			var outfittext = outfit.name;
-			if(outfit.alias !== "") {
-				outfittext = "[" + outfit.alias + "] " + outfittext;
-			}
-			self.$outfit.text(outfittext);
-            self.$outfit.attr('href', 'outfit.html?id=' + outfit.outfit_id);
-		}
-		else {
-			self.$outfit.text('');
-		}
-		self.$br.text(character.battle_rank.value);
-		
-		if(character.characters_online_status.online_status == "0") {
-			$("#online").css('background-color', '#f00');
-		}
-		else {
-			$("#online").css('background-color', '#0f0');
-		}
-		
-		var stats = character.characters_stat_history;
-		
-		var stats2 = [
-			[stats.time.all_time*1, stats.score.all_time*1, stats.kills.all_time*1, stats.deaths.all_time*1, 0, 0, 0]
-			,[0, 0, 0, 0, 0, 0, 0]
-			,[0, 0, 0, 0, 0, 0, 0]
-			,[0, 0, 0, 0, 0, 0, 0]
-		]
-		
-		var sum_time = 0;
-		var sum_score = 0;
-		var sum_kills = 0;
-		var sum_deaths = 0;
-		var start = 0;
-		
-		for(var i = 1; i<=31; ++i) {
-			if(i < 10) {
-				var str_i = 'd0' + i;
-			}
-			else {
-				var str_i = 'd' + i;
-			}
-			
-			sum_time += stats.time.day[str_i]*1;
-			sum_score += stats.score.day[str_i]*1;
-			sum_kills += stats.kills.day[str_i]*1;
-			sum_deaths += stats.deaths.day[str_i]*1;
-			
-			var stat2_i = 0;
-			if(i === 1) {
-				stat2_i = 3;
-			}
-			else if(i === 7) {
-				stat2_i = 2;
-			}
-			else if(i === 31) {
-				stat2_i = 1;
-			}
-			
-			if(stat2_i) {
-				stats2[stat2_i][0] = sum_time;
-				stats2[stat2_i][1] = sum_score;
-				stats2[stat2_i][2] = sum_kills;
-				stats2[stat2_i][3] = sum_deaths;
-			}
-		}
-		
-		for(var i = 0; i<4; ++i) {
-			var time = stats2[i][0] / 60;
-			var score = stats2[i][1];
-			var kills = stats2[i][2];
-			var deaths = stats2[i][3];
-			
-			if (deaths == 0) deaths = 1;
-			if (time == 0) time = 1;
-
-			stats2[i][4] = (kills / deaths).toFixed(2);
-			stats2[i][5] = (score / time).toFixed(0);
-			stats2[i][6] = (kills / time).toFixed(2);
-		}
-		
-		$.each(self.$container.find('#stats_overall').find('td'), function(idx, td) {
-			var value = stats2[0][idx];
-			if(idx === 0) {
-				var duration = moment.duration(value, 's')
-				$(td).text(duration.format());
-			}
-			else {
-				$(td).text(value);
-			}
-		});
-		
-		$.each(self.$container.find('#stats_monthly').find('td'), function(idx, td) {
-			var value = stats2[1][idx];
-			if(idx === 0) {
-				var duration = moment.duration(value, 's')
-				$(td).text(duration.format());
-			}
-			else {
-				$(td).text(value);
-			}
-		});
-		
-		$.each(self.$container.find('#stats_weekly').find('td'), function(idx, td) {
-			var value = stats2[2][idx];
-			if(idx === 0) {
-				var duration = moment.duration(value, 's')
-				$(td).text(duration.format());
-			}
-			else {
-				$(td).text(value);
-			}
-		});
-		
-		$.each(self.$container.find('#stats_daily').find('td'), function(idx, td) {
-			var value = stats2[3][idx];
-			if(idx === 0) {
-				var duration = moment.duration(value, 's')
-				$(td).text(duration.format());
-			}
-			else {
-				$(td).text(value);
-			}
-		});
-		
-		self.$lastupdate.text(moment.unix(stats.score.last_save*1 + 25200).format("MMM D, HH:mm:ss"));
-	}
-	
-	return {
-		init: function init($container) {
-			self.$container = $container;
-			self.$name = self.$container.find('#name').find('span:first')
-			self.$br = self.$container.find('#br').children('span')
-			self.$outfit = self.$container.find('#outfit').children('a')
-			self.$lastupdate = self.$container.find('#lastupdate').children('span')
-		}
-		,update: function update(character_id) {
-			var url = "http://census.soe.com/get/ps2:v2/character/?";
-			url += "c:join=characters_stat_history^list:1^inject_at:characters_stat_history";
-			url += "&c:tree=start:characters_stat_history^field:stat_name";
-			url += "&c:join=outfit_member^on:character_id^inject_at:outfit_member(outfit^inject_at:outfit)";
-			url += "&c:join=characters_online_status^on:character_id^inject_at:characters_online_status^show:online_status"
-			url += "&callback=?";
-
-			$.getJSON(url, { character_id: character_id }, build);
-		}
-	};
-}();
+(function() {
+    var self = this;
+    
+    var url = "http://census.soe.com/get/ps2:v2/character/?";
+    url += "c:join=characters_stat_history^list:1^inject_at:characters_stat_history";
+    url += "&c:tree=start:characters_stat_history^field:stat_name";
+    url += "&c:join=outfit_member^on:character_id^inject_at:outfit_member(outfit^inject_at:outfit)";
+    url += "&c:join=characters_online_status^on:character_id^inject_at:characters_online_status^show:online_status"
+    url += "&callback=?";
+    
+    var viewModel = {
+        characterId: ko.observable()
+      , name: ko.observable('')
+      , br: ko.observable('')
+      , online: ko.observable('')
+      
+      , outfitId: ko.observable()
+      , outfitName: ko.observable('')
+      , outfitAlias: ko.observable('')
+      
+      , statistics: ko.observable(null)
+      , lastUpdate: ko.observable('')
+    };
+    
+    var getCharacter = function() {
+        $.getJSON(url, { character_id: viewModel.characterId() }, build);
+    };
+    
+    var build = function build(census_result) {
+        var character = census_result.character_list[0];
+        
+        viewModel.name(character.name.first);
+        
+        if(character.outfit_member) {
+            var outfit = character.outfit_member.outfit
+            viewModel.outfitId(outfit.outfit_id);
+            viewModel.outfitName(outfit.name);
+            viewModel.outfitAlias(outfit.alias);
+        }
+        
+        viewModel.br(character.battle_rank.value);
+        
+        if(character.characters_online_status) {
+            viewModel.online(character.characters_online_status.online_status);
+        }
+        
+        viewModel.statistics(null);
+        viewModel.lastUpdate('');
+        
+        var characters_stat_history = character.characters_stat_history;
+        if(characters_stat_history) {
+            viewModel.statistics(ps2.stats.compute(characters_stat_history));
+            viewModel.lastUpdate(moment.unix(characters_stat_history.score.last_save*1 + 25200).format("MMM D, HH:mm:ss"));
+        }
+    }
+    
+    viewModel.outfit = ko.computed(function() {
+        var outfit = viewModel.outfitName();
+        var alias = viewModel.outfitAlias();
+        if(alias) {
+            outfit = "[" + alias + "] " + outfit;
+        }
+        return outfit;
+    });
+    
+    if(!window.ps2) window.ps2 = {};
+    window.ps2.player = {
+        update: function(id) {
+            viewModel.characterId(id);
+            getCharacter();
+        }
+    };
+    
+    $(function() {
+        ko.applyBindings(viewModel, document.getElementById('player'));
+    });
+})();
